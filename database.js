139a2da25fc74cebb3312bd1a83a041c23a3a4f0
Winfield Warren <winfieldwarren@unc.edu>
2022-05-02 01:21:59 -0400

    trying

--- database.js
+++ database.js
@@ -1,43 +1,39 @@
-// This ensures that things do not fail silently but will throw errors instead.
-"use strict";
-// Require better-sqlite.
-const Database = require('better-sqlite3');
+// Put your database code here
+const database = require('better-sqlite3')
 
-// Connect to a database or create one if it doesn't exist yet.
-const db = new Database('log.db');
+const fs = require('fs');
+const datadir = './data/';
 
-// Is the database initialized or do we need to initialize it?
-const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
-    );
-// Define row using `get()` from better-sqlite3
+if (!fs.existsSync(datadir)){
+    fs.mkdirSync(datadir);
+}
+
+const logdb = new database(datadir+'log.db')
+
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
-// Check if there is a table. If row is undefined then no table exists.
 if (row === undefined) {
-// Echo information about what you are doing to the console.
-    console.log('Your database appears to be empty. I will initialize it now.');
-// Set a const that will contain your SQL commands to initialize the database.
+    console.log('Log database appears to be empty. Creating log database...')
+
     const sqlInit = `
-        CREATE TABLE accesslog (
-            remoteaddr TEXT, 
-            remoteuser TEXT, 
-            time TEXT, 
-            method TEXT, 
-            url TEXT, 
-            protocol TEXT, 
-            httpversion TEXT, 
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT,
+            remoteuser TEXT,
+            time TEXT,
+            method TEXT,
+            url TEXT,
+            protocol TEXT,
+            httpversion TEXT,
             status TEXT, 
-            referer TEXT, 
+            referrer TEXT,
             useragent TEXT
         );
-    `;
-// Execute SQL commands that we just wrote above.
-    db.exec(sqlInit);
-// Echo information about what we just did to the console.
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+    `
+
+    logdb.exec(sqlInit)
 } else {
-// Since the database already exists, echo that to the console.
-    console.log('Database exists.')
+    console.log('Log database exists.')
 }
-// Export all of the above as a module so that we can use it elsewhere.
-module.exports = db
+
+module.exports = logdb
--- server.js
+++ server.js
@@ -1,128 +1,77 @@
-//import { json } from "express/lib/response";
-//import {coinFlip, coinFlips, countFlips, flipACoin} from "../modules/coin.mjs";
-//import minimist from "minimist";
-//import requirejs from "requirejs";
-
-// Require Express.js
-const morgan = require('morgan')
-const express = require('express')
-const app = express()
-const db = require('./database')
-const fs = require('fs')
-
-const args = require('minimist')(process.argv.slice(2))
-args['port'];
-args['log'];
-const port = args.port || 5000
-const log = args.log || 'true'
-
+// Place your server entry point code here
+// Require minimist module (make sure you install this one via npm).
 // Require minimist module
-//const args = require('minimist')(process.argv.slice(2))
+const args = require('minimist')(process.argv.slice(2))
 // See what is stored in the object produced by minimist
-console.log(args)
+//console.log('Command line arguments: ', args)
 // Store help text 
 const help = (`
 server.js [options]
-
---port	Set the port number for the server to listen on. Must be an integer
+--port, -p	Set the port number for the server to listen on. Must be an integer
             between 1 and 65535.
-
---debug	If set to true, creates endlpoints /app/log/access/ which returns
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
-
 --log		If set to false, no log files are written. Defaults to true.
             Logs are always written to database.
-
---help	Return this message and exit.
+--help, -h	Return this message and exit.
 `)
-// If --help or -h, echo help text to STDOUT and exit
+// If --help, echo help text and exit
 if (args.help || args.h) {
     console.log(help)
     process.exit(0)
 }
+// Define app using express
+var express = require('express')
+var app = express()
+// Require fs
+const fs = require('fs')
+// Require morgan
+const morgan = require('morgan')
+// Require database SCRIPT file
+const logdb = require('database.js')
+// Make Express use its own built-in body parser
+// Allow urlencoded body messages
+//app.use(express.urlencoded({ extended: true }));
+// Allow json body messages
+app.use(express.json());
+// Server port
+const port = args.port || args.p || process.env.PORT || 5000
+// If --log=false then do not create a log file
+if (args.log == 'false') {
+    console.log("NOTICE: not creating file access.log")
+} else {
+// Use morgan for logging to files
+    const logdir = './log/';
 
-// Start an app server
-const server = app.listen(port, () => {
-  console.log('App listening on port %PORT%'.replace('%PORT%',port))
-});
-
-if (log == 'true') {
-  // Use morgan for logging to files
-// Create a write stream to append (flags: 'a') to a file
-const WRITESTREAM = fs.createWriteStream('FILE', { flags: 'a' })
+    if (!fs.existsSync(logdir)){
+        fs.mkdirSync(logdir);
+    }
+// Create a write stream to append to an access.log file
+    const accessLog = fs.createWriteStream( logdir+'access.log', { flags: 'a' })
 // Set up the access logging middleware
-app.use(morgan('FORMAT', { stream: WRITESTREAM }))
+    app.use(morgan('combined', { stream: accessLog }))
 }
-
-app.use( (req, res, next) => {
-  // Your middleware goes here.
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
-  }
-
-  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
-  const info = db.run(logdata.remoteaddr, logdata.remoteuser, logdata.rime, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-  next()
+// Always log to database
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referrer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    };
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    //console.log(info)
+    next();
 })
-
-app.get('/app/log/access', (req, res) => {
-  try {
-    const stmt = db.prepare(`SELECT * FROM accesslog`).all()
-    res.status(200).json(stmt)
-  }
-  catch {
-    console.error(e)
-  }
-})
-
-app.get('/app/error/', (req, res) => {
-  throw new Error("Error test successful")
-})
-
-
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-    res.statusCode = 200;
-    // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
-});
-
-app.get('/app/flip/', (req, res) => {
-    const flip = coinFlip();
-    res.status(200).json({"flip": flip});
-});
-
-app.get('/app/flips/:number', (req, res) => {
-    const flips = coinFlips(req.params.number);
-    res.status(200).json({"raw": flips, "summary": countFlips(flips)})
-    //Some
-    //expressions
-    //go
-    //here
-});
-
-app.get('/app/flip/call/:string', (req, res) => {
-    const call = flipACoin(req.params.string);
-    res.status(200).json(call);
-});
-
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
 
 /** Coin flip functions 
  * This module will emulate a coin flip given various conditions as parameters as defined below
@@ -246,3 +195,55 @@ app.use(function(req, res){
     }
     return {call: call, flip: resultingflip, result: results};
   }
+  
+  
+  /** Export 
+   * 
+   * Export all of your named functions
+  */
+  
+
+// Serve static HTML public directory
+app.use(express.static('./public'))
+
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
+});
+
+app.get('/app/flip/', (req, res) => {
+  const flip = coinFlip();
+  res.status(200).json({"flip": flip});
+});
+
+app.get('/app/flips/:number', (req, res) => {
+  const flips = coinFlips(req.params.number);
+  res.status(200).json({"raw": flips, "summary": countFlips(flips)})
+  //Some
+  //expressions
+  //go
+  //here
+});
+
+app.get('/app/flip/call/:string', (req, res) => {
+  const call = flipACoin(req.params.string);
+  res.status(200).json(call);
+});
+
+if (args.debug || args.d) {
+    app.get('/app/log/access/', (req, res, next) => {
+        const stmt = logdb.prepare("SELECT * FROM accesslog").all();
+	    res.status(200).json(stmt);
+    })
+
+    app.get('/app/error/', (req, res, next) => {
+        throw new Error('Error test works.')
+    })
+}
+
+
+// Start server
+const server = app.listen(port, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",port))
+});
